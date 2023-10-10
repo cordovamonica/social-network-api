7541353cefaf2dcc7246ccbe312522ff50e1ecb8
Mona Lisa <cordovamonica.a@gmail.com>
2023-10-10 10:56:13 -0500

    fixed connection bug

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetworkDB';
+const connectionString = process.env.MONGODB_URI || 'mongodb+srv://cordovamonica:Skrll_cat1@cluster0.tqdjvj8.mongodb.net/socialnetworkDB';
 
 connect(connectionString);
 
